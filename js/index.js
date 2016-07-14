6210c6476f4cb3d392db4238f686a7896027bc00
Leon Li <leon@Leons-MacBook-Pro.local>
2016-07-14 01:40:47 -0700

    add multi video support

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 var vid = document.getElementById("bgvid");
+var vidsources = document.getElementsById("vidsrc");
 
 function gotoCoffee(){
+	
 	window.location='coffee.html';
+}
+
+function putVideo(){
+
+	vidID = randInt(1,3).toString();
+	vidsrc[0].src = 'res/flickbean' + vidID + '.webm';
+	vidsrc[1].src = 'res/flickbean' + vidID + '.mp4';
+	vid.load();
+	vid.play();
+}
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 vid.addEventListener('ended', function(){
