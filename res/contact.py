e3e652e8a46eec64f290844c79d128e787d63e07
Leon Li <leontosy@gmail.com>
2016-07-24 19:04:08 -0700

    fix contact issues

--- res/contact.py
+++ res/contact.py
@@ -5,6 +5,12 @@ email=form.getvalue('email')
 subject=form.getvalue('subject')
 message=form.getvalue('message')
 
-line=email+'|'+subject+'|'+message
-with open('res/contacts.txt','a') as f:
-    f.write(line)
+print 'Content-type: text/html\r\n'
+
+try:
+	line=email+'|'+subject+'|'+message+'\n'
+	with open('contacts.txt','a') as f:
+	    f.write(line)
+	print 'Your information has been submitted. <a href="../coffee.html">Click here</a> to return.'
+except:
+	print '<script>window.location = "../contact.html";'
