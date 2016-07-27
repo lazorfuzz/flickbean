d995cd5bffb939a8d708bbe338d281633a09aa56
Leon Li <leontosy@gmail.com>
2016-07-26 18:33:53 -0700

    bug fixes

--- js/coffee.js
+++ js/coffee.js
@@ -109,15 +109,9 @@ function submitContact(){
 			return;
 			}
 		}
-		
-	document.forms["contact"].submit();
+		document.getElementById('contact').submit();
 	document.getElementById('confirmation').style = 'display:block; color: rgba(102, 255, 102, 0.7); text-align:center;';
 	document.getElementById('contactbutton').style='display:none;';
-
-	for (i = 0; i < inputs.length; i++){
-		inputs[i].value = '';
-	}
-	document.getElementById('message').value = '';
 }
 
 function inArray(value, array) {
